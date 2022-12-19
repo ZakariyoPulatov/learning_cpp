5ee7ef5e4b0d9caa5986e3380e2c01fd2539d579
zakariyo <zakariyopulatov001@gmail.com>
2022-12-19 21:25:45 +0500

    2

---
+++ 01_kiritish_va_chiqarishga_doir_masalalar/2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main ()
+{  
+   int a, b, c;
+
+   cout << "a="; cin >> a;
+   cout << "b="; cin >> b;
+    
+   c = a + b;
+
+   cout <<"a+b=" << c<< endl;
+
+   return 0;
+
+
+
+}
