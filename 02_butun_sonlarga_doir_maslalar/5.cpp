903de8f15bdaa9e5bef897727ac1a7cc5750a9ef
zakariyo <zakariyopulatov001@gmail.com>
2022-12-19 21:57:06 +0500

    5

---
+++ 02_butun_sonlarga_doir_maslalar/5.cpp
@@ -0,0 +1,18 @@
+//Sana; 2022.12.19
+//Muallif; Zakariyo Pulatov
+//Maqsad;
+# include <iostream>
+# include <math.h>
+using namespace std;
+
+int main ()
+{
+   int a, b;
+
+   cout << "a= "; cin >> a;
+   cout << "b= "; cin >> b;
+
+   cout << a << " kesmada " << b << " kesmadan " << a / b << " ta bor endl ";
+
+   return 0;
+}
