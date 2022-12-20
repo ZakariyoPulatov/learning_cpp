22c210e5339cf06ebc31b8843a3ee08666ae3e11
zakariyo <zakariyopulatov001@gmail.com>
2022-12-20 12:15:38 +0500

    9

---
+++ 02_butun_sonlarga_doir_maslalar/8.cpp
@@ -0,0 +1,27 @@
+//Sana; 2022.12.20
+//Muallif; Zakariyo Pulatov
+//Maqsad; Ikki xonali son berilgan. Uning raqamlarini o'rnini almashtrishdan hosil bo'lgan sonni aniqlovchi Pr tuzilsin
+
+# include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a, b, c;
+
+    cout << "a= "; cin >> a;
+    cout << "b= "; cin >> b;
+    cout << 'c= '; cin >> c;
+
+
+    cout << " a=" << endl;
+
+    return 0;
+
+    cout <<
+
+
+
+
+}
---
+++ 02_butun_sonlarga_doir_maslalar/9.cpp
@@ -0,0 +1,20 @@
+//Sana; 2022.20.12
+//Muallif; Zakariyo Pulatov
+//Maqsad; Uch xonalik son berilgan. Uning yuzlar xonasidagi raqamini aniqlovchi Pr tuzilsin
+
+# include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int abc;
+
+    cout << "abc= "; cin >> abc;
+
+    cout << " abc= " << "yuzlar xonasidagi raqam= " << abc / 100 << endl;
+
+    return 0;
+
+
+}
