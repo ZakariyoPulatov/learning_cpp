b86c2e97e1855b27bb02e6446d0aa99e11145887
ZakariyoPulatov <120955342+ZakariyoPulatov@users.noreply.github.com>
2022-12-20 10:10:10 +0500

    6.cpp

--- 02_butun_sonlarga_doir_maslalar/6.cpp
+++ 02_butun_sonlarga_doir_maslalar/6.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main ()
 {   
-    int ab;
+    int ab, a;
 
     cout << "ab= "; cin >> ab;
+    cout << "a= "; cin >>a;
      
     cout <<"ab= " << "o'nlar xonasidagi raqam " << ab / 10 << endl;
-
+    cout <<"ab= " << "o'nlar xonasidagi raqam " << ab % a<< endl;
+    
 return 0;
 
 }
