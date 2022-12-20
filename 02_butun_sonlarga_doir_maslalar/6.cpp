9a4db49bf8708026855b2b0b336b7b8c6fb8d76e
zakariyo <zakariyopulatov001@gmail.com>
2022-12-20 09:26:16 +0500

    6

---
+++ 02_butun_sonlarga_doir_maslalar/6.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <math.h>
+
+using namespace std;
+
+int main ()
+{   
+    int ab;
+
+    cout << "ab= "; cin >> ab;
+     
+    cout <<"ab= " << "o'nlar xonasidagi raqam " << ab / 10 << endl;
+
+return 0;
+
+}
