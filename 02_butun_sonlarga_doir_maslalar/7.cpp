addfc14ed5923400232d3a8ed6d961677989bdc6
zakariyo <zakariyopulatov001@gmail.com>
2022-12-20 11:33:26 +0500

    7.cpp

---
+++ 02_butun_sonlarga_doir_maslalar/7.cpp
@@ -0,0 +1,24 @@
+//Sana; 2022.12.20
+//Mullif; Zakariyo Pulatov
+//Maqsad; Ikki xonali son berilgan. Uning raqamlar yig'indisini aniqlovchi Pr tuzilsin
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a, b;
+
+    cout << "a= "; cin >> a;
+    cout << "b= "; cin >> b;
+
+
+    cout << "a + b= " << a + b <<  endl;
+
+    return 0;
+
+
+
+
+
+}
