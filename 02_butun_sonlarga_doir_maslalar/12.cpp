f2318051ee2a685ce11823d8ed1ecf5ed1168dd9
zakariyo <zakariyopulatov001@gmail.com>
2022-12-21 11:20:20 +0500

    12.cpp

--- 02_butun_sonlarga_doir_maslalar/12.cpp
+++ 02_butun_sonlarga_doir_maslalar/12.cpp
@@ -1,15 +1,15 @@
-//Sana; 2022.12.20
-//Muallif; zakariyo  Pulatov
-//Maqsad; 
+//Sana: 2022.12.20
+//Muallif: zakariyo  Pulatov
+//Maqsad: Uch xonali son berilgan. Uning raqamlarini teskari tartibda yozishdan hosil bo'lgan sonni aniqlovchi Pr tuzilsin
 #include <iostream>
 
 using namespace std;
 
 int main ()
 {
-    int abc,
+    int son = 456;
+
+    cout << son << " sonning teskari ko'rinishi= " << (son/100) + (son%100/10*10) + (son%10*100) << endl;
 
     return 0;
-
-
 }
