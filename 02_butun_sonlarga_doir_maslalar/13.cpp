fb0d9dc79bf2d8c7e191beb5c164148c4f543800
zakariyo <zakariyopulatov001@gmail.com>
2022-12-21 11:35:39 +0500

    13.cpp

---
+++ 02_butun_sonlarga_doir_maslalar/13.cpp
@@ -0,0 +1,15 @@
+//Sana: 2022.12.21
+//Muallif: Zakariyo Pulatov
+//Maqsad: Uch xonali son berilgan. Uning chapdan birinchi raqmini o'chirib o'ng tarafiga yozishdan hosil bo'lgan sonni aniqlocvhi Pr tuzilsi
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int son = 345;
+
+    cout << son << "korinish= " << (son/100) + (son%100/10*10) + (son%10*100) << endl;
+
+    return 0;
+}
