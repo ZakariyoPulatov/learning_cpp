5edede1aa8929783a9eb705d3ad6ce53733e509c
zakariyo <zakariyopulatov001@gmail.com>
2022-12-21 12:06:21 +0500

    14.cpp

---
+++ 02_butun_sonlarga_doir_maslalar/14.cpp
@@ -0,0 +1,15 @@
+//Sana: 2022.12.21
+//Muallif: Zakariyo Pulatov 
+//Maqsad: Uch xonali son berilgan. O'ngdan birinchi raqamini o'chirib chap tarafiga yozishdan hosil bo'lgan sonni aniqlovchi Pr tuzilsin
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int son = 567;
+
+    cout << son << "_ko'rinish_= " << (son/100) + (son%100/10*10) + (son%10*100) << endl;
+
+    return 0;
+}
