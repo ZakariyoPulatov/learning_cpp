dbefed595c0faf3706e1c7ea2804b956611dbd53
zakariyo <zakariyopulatov001@gmail.com>
2022-12-21 15:34:29 +0500

    18

---
+++ 02_butun_sonlarga_doir_maslalar/18.cpp
@@ -0,0 +1,16 @@
+//Sana: 2022.12.21
+//Muallif: Zakariyo Pulatov
+//Maqsad: 999 dan katta bo'lgan son berilgan. Bir martta bo'lib butunni va bo'lib qoldiqni olish operatsiyasidan foydalanib
+//berilgan sonni mingliklar xonasidagi sonni aniqlovchi Pr tuzilsin
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int kirish = 1234;
+
+    cout << kirish << " chiqish= " << (kirish/1000) << endl;
+
+    return 0;
+}
