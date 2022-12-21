2d27957ee501b7cd81d555a3e6b7b868c7d48f72
zakariyo <zakariyopulatov001@gmail.com>
2022-12-21 12:30:17 +0500

    15

---
+++ 02_butun_sonlarga_doir_maslalar/15.cpp
@@ -0,0 +1,16 @@
+//Sana: 2022.12.21
+//Muallif: Zakariyo Pulatov
+//Maqsad: Uch xonali son berilgan. Uning o'nliklar xonasidagiraqam bilan yuzliklar xonasidagi 
+//raqamni almashtirishdan hosil bo'lgan sonni aniqlovchi Pr tuzilsin (kirish=123; natija=213)
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int kirish = 123;
+
+    cout << kirish << " natija = " << (kirish/100*10) + (kirish%100/10*100) + (kirish%10) << endl;
+    
+    return 0;
+}
