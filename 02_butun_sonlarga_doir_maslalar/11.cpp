d3f2593c3f4542cfeb72aa2bf5c7297cabe13ad1
zakariyo <zakariyopulatov001@gmail.com>
2022-12-21 11:05:23 +0500

    11.cpp

--- 02_butun_sonlarga_doir_maslalar/11.cpp
+++ 02_butun_sonlarga_doir_maslalar/11.cpp
@@ -1,17 +1,15 @@
-//Sana; 2022.12.20
-//Muallif; Zakariyo Pulatov
-//Maqsad; Uchxonali son berilgan.  Uning raqamlar yig'indisini aniqlovchi Pr tuzilsin
+//Sana: 2022.12.20
+//Muallif: Zakariyo Pulatov
+//Maqsad: Uchxonali son berilgan. Uning raqamlar yig'indisini aniqlovchi Pr tuzilsin
 #include <iostream>
 
 using namespace std;
 
 int main () 
 {
-    int abc;
+    int son = 123;
 
-    cout <<"abc=
-
-    cout <<"abc sonlar yig'indisi topish= " << a + b +c << endl;
+    cout <<son << "raqamlar yig'indisi= " << (son/100) + (son%100/10) + (son%10)  << endl;
 
     return 0;
 }
