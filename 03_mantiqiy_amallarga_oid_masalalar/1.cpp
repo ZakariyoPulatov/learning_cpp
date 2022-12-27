40c6cbfc8b52625eeba2962ed8c71c76edea1ce4
zakariyo <zakariyopulatov001@gmail.com>
2022-12-27 18:21:29 +0500

    bajarildi

---
+++ 03_mantiqiy_amallarga_oid_masalalar/1.cpp
@@ -0,0 +1,15 @@
+//Sana: 2022.12.27
+//Muallif: Zakariyo Pulatov
+//Maqsad: a butun son berilgan. Jumlani rostlikka tekshiring "a soni musbat"
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a;
+    bool rost;
+
+    cout << a << r
+
+}
