1f25b1f7bae623ffdad457cb2f68d05acf0000d4
zakariyo <zakariyopulatov001@gmail.com>
2022-12-27 18:10:20 +0500

    14.cpp

---
+++ 01_kiritish_chiqarish_va_o'zlashtirish_operatoriga_oid_masalalar/1.cpp
@@ -0,0 +1,15 @@
+//Sana: 2022.12.21
+//Muallif: Zakariyo Pulatov
+//Maqsad: Kvadratning tamoni a berilgan. Uning permetri aniqlansin. P = 4 * a
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 5;
+
+    cout << " p = " << 4 * a  << endl;
+
+    return 0;
+}
