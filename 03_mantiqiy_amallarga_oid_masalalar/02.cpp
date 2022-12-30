cf4f9176c25449c4f80e68ee61cda51a67659271
zakariyo <zakariyopulatov001@gmail.com>
2022-12-30 19:00:49 +0500

    02

---
+++ 03_mantiqiy_amallarga_oid_masalalar/02.cpp
@@ -0,0 +1,16 @@
+// Sana: 2022.12.30
+// Muallif: Zakariyo Pulatov
+// Maqsad: a butun son berilgan. Jumlani rostlikka tekshiring: " a soni toq son ".
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 3;
+    bool natija;
+
+    cout << " natija = " << ( a % 2 ) << endl;
+
+    return 0;
+}
