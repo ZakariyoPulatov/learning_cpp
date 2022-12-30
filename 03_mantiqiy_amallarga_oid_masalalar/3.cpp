c34e6d2c057da58d4be2749412e8caf487be66f4
zakariyo <zakariyopulatov001@gmail.com>
2022-12-30 17:44:15 +0500

    3

---
+++ 03_mantiqiy_amallarga_oid_masalalar/3.cpp
@@ -0,0 +1,16 @@
+//Sana: 2022.12.30
+//Muallif: Zakariyo Pulatov
+//Maqsad: a butun son berilgan. Jumlani rostlikka tekshiring: " a soni juft son "
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 2;
+    bool natija;
+
+    cout << " natija = " << !( a % 2 ) << endl;
+
+    return 0;
+}
