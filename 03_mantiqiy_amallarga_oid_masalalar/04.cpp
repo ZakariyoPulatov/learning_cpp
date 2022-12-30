e72d5b95ca51dbf721f0ececeb8c56d049630b2f
zakariyo <zakariyopulatov001@gmail.com>
2022-12-30 19:11:46 +0500

    04

---
+++ 03_mantiqiy_amallarga_oid_masalalar/04.cpp
@@ -0,0 +1,16 @@
+// Sana: 2022.12.30
+// Muallif: Zakariyo Pulatov
+// Maqsad: a va b sonlari berilgan. Jumlani rostlikka tekshiring: " a>2 va b<=3 ".
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 3, b = 2;
+    bool natija;
+
+    cout << " natija a = " << ( a > 2 ) << " natija b = " << ( b <= 3 ) << endl;
+
+    return 0;
+}
