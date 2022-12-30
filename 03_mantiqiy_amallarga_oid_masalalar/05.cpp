8d9781fedc7c3c772531b8f761d808bfea25011b
zakariyo <zakariyopulatov001@gmail.com>
2022-12-30 19:21:13 +0500

    05

---
+++ 03_mantiqiy_amallarga_oid_masalalar/05.cpp
@@ -0,0 +1,16 @@
+// Sana: 2022.12.30
+// Muallif: Zakariyo Pulatov
+// Maqsad: Ikkita butun a va b sonlari berilgan. Jumlani rostlikka tekshiring: " a>=0 va b<-2 ".
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 1, b = -3;
+    bool natija;
+
+    cout << " natija a = " << ( a >= 0 ) << " natija b = " << ( b < -2 ) << endl;
+
+    return 0;
+}
