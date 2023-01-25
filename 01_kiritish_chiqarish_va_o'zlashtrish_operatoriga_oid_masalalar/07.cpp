e7c903b01138a8bfbe645f6c4d6149a14c858ca3
zakariyo <zakariyopulatov001@gmail.com>
2023-01-25 22:23:32 +0500

    ok

--- 01_kiritish_chiqarish_va_o'zlashtrish_operatoriga_oid_masalalar/07.cpp
+++ 01_kiritish_chiqarish_va_o'zlashtrish_operatoriga_oid_masalalar/07.cpp
@@ -6,12 +6,37 @@
 
 using namespace std;
 
+void L_1();
+void L_2(int r);
+void S_1();
+void S_2(int r);
+
 int main()
 {
-    int R = 5;
-
-    cout << " L = " << 2 * 3.14 * R << endl;
-    cout << " S = " << 3.14 * R << endl;
+    L_1();
+    L_2(10);
+    S_1();
+    S_2(10);
 
     return 0;
+}
+void L_1() {
+
+    int r = 10;
+
+    cout << " L_1 = " << 2 * 3.14 * r << endl;
+}
+void L_2(int r) {
+
+    cout << " L_2 = " << 2 * 3.14 * r << endl;
+}
+void S_1() {
+
+    int r = 10;
+
+    cout << " S_1 = " << 3.14 * r << endl;
+}
+void S_2(int r) {
+
+    cout << " S_2 = " << 3.14 * r << endl;
 }
